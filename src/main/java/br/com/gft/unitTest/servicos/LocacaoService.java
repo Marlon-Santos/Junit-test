4bb8d50f53e0355a07dc53c0407e8aa699cb4c81
Marlon Santos <marlonergondossantos@gmail.com>
2020-03-17 15:18:54 -0300

    folder organization

--- src/main/java/br/com/gft/unitTest/servicos/LocacaoService.java
+++ src/main/java/br/com/gft/unitTest/servicos/LocacaoService.java
@@ -6,8 +6,6 @@ import br.com.gft.unitTest.entidades.Filme;
 import br.com.gft.unitTest.entidades.Locacao;
 import br.com.gft.unitTest.entidades.Usuario;
 import br.com.gft.unitTest.utils.DataUtils;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class LocacaoService {
 
@@ -27,15 +25,5 @@ public class LocacaoService {
         //TODO adicionar método para salvar
 
         return locacao;
-    }
-    @Test
-    public void test() {
-        LocacaoService locacaoService = new LocacaoService();
-        Usuario u1 = new Usuario("malon ergon dos santos");
-        Filme f1 = new Filme("Avangers", 10, 10.00);
-        Locacao l1 = locacaoService.alugarFilme(u1, f1);
-        Assert.assertTrue(DataUtils.isMesmaData(l1.getDataLocacao(), new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), l1.getDataRetorno()));
-        Assert.assertTrue(l1.getValor() == 10);
     }
 }
