3d956766f87afa2971164a327b293cce4618daba
Marlon Santos <marlonergondossantos@gmail.com>
2020-03-18 14:03:55 -0300

    testing with Before and After Anotation

---
+++ src/test/java/br/com/gft/unitTest/servicos/AfterAndBefore.java
@@ -0,0 +1,41 @@
+package br.com.gft.unitTest.servicos;
+
+import org.junit.*;
+
+public class AfterAndBefore {
+
+    private static int cont = 0;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("antes de instancia a classe");
+    }
+
+    @Before
+    public void before() {
+        cont ++;
+        System.out.println("before: " + cont);
+
+    }
+
+    @Test
+    public void test() {
+        System.out.println("current value: " + cont);
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("current value1: " + cont);
+    }
+
+    @After
+    public void after() {
+        cont ++;
+        System.out.println("after: " + cont);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("depois de matar a classe");
+    }
+}
