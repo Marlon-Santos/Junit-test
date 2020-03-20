26c55ee420a9fe3d92d6b5daaa65a2c4614d4f75
Marlon Santos <marlonergondossantos@gmail.com>
2020-03-20 12:25:16 -0300

    working with data builders

---
+++ src/main/java/br/com/gft/unitTest/builders/LocacaoBuilder.java
@@ -0,0 +1,38 @@
+package br.com.gft.unitTest.builders;
+
+import java.util.Arrays;
+
+import br.com.gft.unitTest.entidades.Filme;
+import br.com.gft.unitTest.entidades.Usuario;
+import br.com.gft.unitTest.entidades.Locacao;
+import br.com.gft.unitTest.exceptions.LocacaoServiceException;
+import br.com.gft.unitTest.servicos.LocacaoService;
+
+public class LocacaoBuilder {
+    private LocacaoService elemento;
+    static Locacao locations;
+
+    private LocacaoBuilder() {
+    }
+
+    public static LocacaoBuilder locate() throws LocacaoServiceException {
+        LocacaoBuilder builder = new LocacaoBuilder();
+        inicializarDadosPadroes(builder);
+        return builder;
+    }
+
+    public static void inicializarDadosPadroes(LocacaoBuilder builder) throws LocacaoServiceException {
+        builder.elemento = new LocacaoService();
+        Usuario u1 = new Usuario("malon ergon dos santos");
+        Filme f1 = new Filme("Avangers", 10, 10.00);
+        Filme f2 = new Filme("Spider-man", 10, 10.00);
+        Filme f3 = new Filme("Avangers ultimato", 10, 10.00);
+        LocacaoService elemento = builder.elemento;
+        Locacao l1 = new LocacaoService().alugarFilme(u1, Arrays.asList(f1, f2, f3));
+        locations = l1;
+    }
+
+    public Locacao locacao() {
+        return locations;
+    }
+}
--- src/test/java/br/com/gft/unitTest/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/gft/unitTest/servicos/LocacaoServiceTest.java
@@ -1,9 +1,12 @@
 package br.com.gft.unitTest.servicos;
 
+import br.com.gft.unitTest.builders.LocacaoBuilder;
 import br.com.gft.unitTest.entidades.Filme;
 import br.com.gft.unitTest.entidades.Locacao;
 import br.com.gft.unitTest.entidades.Usuario;
 import br.com.gft.unitTest.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,17 +17,11 @@ public class LocacaoServiceTest {
     @Test
     public void test() {
         try {
-            LocacaoService locacaoService = new LocacaoService();
-            Usuario u1 = new Usuario("malon ergon dos santos");
-            Filme f1 = new Filme("Avangers", 10, 10.00);
-            Filme f2 = new Filme("Spider-man", 10, 10.00);
-            Filme f3 = new Filme("Avangers ultimato", 10, 10.00);
-            Locacao l1 = locacaoService.alugarFilme(null, Arrays.asList(f1,f2,f3));
-            Assert.assertTrue(DataUtils.isMesmaData(l1.getDataLocacao(), new Date()));
-            Assert.assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), l1.getDataRetorno()));
-            Assert.assertTrue(l1.getValor() == 10);
-        }catch (Exception e){
-             e.getStackTrace();
+            Assert.assertTrue(DataUtils.isMesmaData(LocacaoBuilder.locate().locacao().getDataLocacao(), new Date()));
+            Assert.assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), LocacaoBuilder.locate().locacao().getDataRetorno()));
+            MatcherAssert.assertThat(LocacaoBuilder.locate().locacao().getValor(), CoreMatchers.equalTo(27.5));
+        } catch (Exception e) {
+            e.getStackTrace();
         }
     }
 }
